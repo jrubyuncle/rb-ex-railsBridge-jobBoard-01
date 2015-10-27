ab1ca97720d1df9bde68fc39bb140a3def0333fe
wesleychu <jrubyuncle@gmail.com>
2015-10-27 11:58:40 +0800

    implement create action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,24 @@
 class JobsController < ApplicationController
 	def index
+		@jobs = Job.all
 	end
 
 	def new
 		@job = Job.new
+	end
+
+    def create
+      @job = Job.new job_params
+      if @job.save
+        redirect_to jobs_path
+      else
+        render :create
+    end
+
+	end
+
+	private
+	def job_params
+		params.require(:job).permit(:title, :description)
 	end
 end
